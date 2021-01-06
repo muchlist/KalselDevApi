69bc4199a482bc6911caba8e6f85bfcc5b012453
mrtsdc <whois.muchlis@gmail.com>
2021-01-06 22:16:38 +0800

    user dto added

---
+++ dto/user_dto.go
@@ -0,0 +1,104 @@
+package dto
+
+import (
+	"github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//User struct lengkap dari document user di Mongodb
+type User struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Email     string             `json:"email" bson:"email"`
+	Name      string             `json:"name" bson:"name"`
+	IsAdmin   bool               `json:"is_admin" bson:"is_admin"`
+	Avatar    string             `json:"avatar" bson:"avatar"`
+	HashPw    string             `json:"hash_pw,omitempty" bson:"hash_pw,omitempty"`
+	Timestamp int64              `json:"timestamp" bson:"timestamp"`
+}
+
+//UserResponseList tipe slice dari UserResponse
+type UserResponseList []UserResponse
+
+//UserResponse struct kembalian dari MongoDB dengan menghilangkan hashPassword
+type UserResponse struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	Email     string             `json:"email" bson:"email"`
+	Name      string             `json:"name" bson:"name"`
+	IsAdmin   bool               `json:"is_admin" bson:"is_admin"`
+	Avatar    string             `json:"avatar" bson:"avatar"`
+	Timestamp int64              `json:"timestamp" bson:"timestamp"`
+}
+
+//UserRequest input JSON untuk keperluan register, timestamp dapat diabaikan
+type UserRequest struct {
+	Email     string `json:"email" bson:"email"`
+	Name      string `json:"name" bson:"name"`
+	IsAdmin   bool   `json:"is_admin" bson:"is_admin"`
+	Avatar    string `json:"avatar" bson:"avatar"`
+	Password  string `json:"password" bson:"password"`
+	Timestamp int64  `json:"timestamp" bson:"timestamp"`
+}
+
+//Validate input
+func (u UserRequest) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Name, validation.Required),
+		validation.Field(&u.Password, validation.Required, validation.Length(3, 20)),
+	)
+}
+
+//UserEditRequest input JSON oleh admin untuk mengedit user
+type UserEditRequest struct {
+	Name            string `json:"name" bson:"name"`
+	IsAdmin         bool   `json:"is_admin" bson:"is_admin"`
+	TimestampFilter int64  `json:"timestamp_filter" bson:"timestamp"`
+}
+
+func (u UserEditRequest) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Name, validation.Required),
+		validation.Field(&u.TimestampFilter, validation.Required),
+	)
+}
+
+//UserLoginRequest input JSON oleh client untuk keperluan login
+type UserLoginRequest struct {
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+//Validate input
+func (u UserLoginRequest) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required, validation.Length(3, 20)),
+	)
+}
+
+//UserChangePasswordRequest struck untuk keperluan change password dan reset password
+//pada reset password hanya menggunakan NewPassword dan mengabaikan Password
+type UserChangePasswordRequest struct {
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	NewPassword string `json:"new_password"`
+}
+
+//Validate input
+func (u UserChangePasswordRequest) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required, validation.Length(3, 20)),
+		validation.Field(&u.NewPassword, validation.Required, validation.Length(3, 20)),
+	)
+}
+
+//UserLoginResponse balikan user ketika sukses login dengan tambahan AccessToken
+type UserLoginResponse struct {
+	Email       string `json:"email" bson:"email"`
+	Name        string `json:"name" bson:"name"`
+	IsAdmin     bool   `json:"is_admin" bson:"is_admin"`
+	Avatar      string `json:"avatar" bson:"avatar"`
+	AccessToken string `json:"access_token"`
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/muchlist/KalselDevApi
 go 1.15
 
 require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/gofiber/fiber/v2 v2.3.2
 	github.com/muchlist/erru_utils_go v1.0.4
 	go.mongodb.org/mongo-driver v1.4.4
