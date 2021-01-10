22b683fd94c09a7343c949e511257be5ed01778e
muchlist <whois.muchlis@gmail.com>
2021-01-10 16:31:09 +0700

    middleware jwt impl

---
+++ middleware/jwt.go
@@ -0,0 +1,97 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/muchlist/KalselDevApi/utils/mjwt"
+	"github.com/muchlist/erru_utils_go/rest_err"
+	"strings"
+)
+
+const (
+	headerKey = "Authorization"
+	bearerKey = "Bearer"
+)
+
+type role int
+
+const (
+	normalAuth role = iota
+	freshAuth
+	adminAuth
+)
+
+//AuthMiddleware memvalidasi token JWT, mengembalikan claims berupa pointer mjwt.CustomClaims
+func AuthMiddleware(c *fiber.Ctx) error {
+	authHeader := c.Get(headerKey)
+	claims, err := authValidator(authHeader, normalAuth)
+	if err != nil {
+		return c.Status(err.Status()).JSON(err)
+	}
+
+	c.Locals(mjwt.CLAIMS, claims)
+	return c.Next()
+}
+
+//AuthMustFreshMiddleware memvalidasi token JWT, mengembalikan claims berupa pointer mjwt.CustomClaims.
+//perbedaannya dengan AuthMidlleware adalah ini mengharuskan pengakses berstatus fresh true
+func AuthMustFreshMiddleware(c *fiber.Ctx) error {
+	authHeader := c.Get(headerKey)
+	claims, err := authValidator(authHeader, freshAuth)
+	if err != nil {
+		return c.Status(err.Status()).JSON(err)
+	}
+
+	c.Locals(mjwt.CLAIMS, claims)
+	return c.Next()
+}
+
+//AuthAdminMiddleware memvalidasi token JWT, mengembalikan claims berupa pointer mjwt.CustomClaims.
+//perbedaannya dengan AuthMidlleware adalah ini mengharuskan pengakses berstatus is_admin true
+func AuthAdminMiddleware(c *fiber.Ctx) error {
+	authHeader := c.Get(headerKey)
+	claims, err := authValidator(authHeader, adminAuth)
+	if err != nil {
+		return c.Status(err.Status()).JSON(err)
+	}
+
+	c.Locals(mjwt.CLAIMS, claims)
+	return c.Next()
+}
+
+func authValidator(authHeader string, role role) (*mjwt.CustomClaim, rest_err.APIError) {
+	if !strings.Contains(authHeader, bearerKey) {
+		apiErr := rest_err.NewUnauthorizedError("Unauthorized")
+		return nil, apiErr
+	}
+
+	tokenString := strings.Split(authHeader, " ")
+	if len(tokenString) != 2 {
+		apiErr := rest_err.NewUnauthorizedError("Unauthorized")
+		return nil, apiErr
+	}
+
+	token, err := mjwt.Obj.ValidateToken(tokenString[1])
+	if err != nil {
+		return nil, err
+	}
+
+	claims, err := mjwt.Obj.ReadToken(token)
+	if err != nil {
+		return nil, err
+	}
+
+	switch role {
+	case adminAuth:
+		if !claims.IsAdmin {
+			apiErr := rest_err.NewUnauthorizedError("Unauthorized, memerlukan hak akses admin")
+			return nil, apiErr
+		}
+	case freshAuth:
+		if !claims.Fresh {
+			apiErr := rest_err.NewUnauthorizedError("Rotten token, memerlukan token yang baru untuk mengakses halaman ini")
+			return nil, apiErr
+		}
+	}
+
+	return claims, nil
+}
